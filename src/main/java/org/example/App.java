8f2a28d7394c8719f32ef2e12d840f4a75e4edc9
luis.gomes <luis.gomes@objective.com.br>
2023-06-27 08:55:48 -0300

    Increasing the tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,22 @@
 package org.example;
 
+import org.example.stack.Stack;
 import org.example.stack.StackImpl;
 
 import java.util.Scanner;
 
 public class App {
     public static void main( String[] args ) {
-        StackImpl stack = new StackImpl();
+        //decimalToBinaryUsingStack();
+        System.out.println("\nThe end of program!");
+    }
+
+    private static void decimalToBinaryUsingStack() {
+        Stack stack = new StackImpl(100);
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a decimal number, to be converted to binary: ");
         int numberInDecimal = scanner.nextInt();
+
         while (numberInDecimal != 0) {
             if(!stack.isFull())
                 stack.push(numberInDecimal % 2);
@@ -18,6 +26,5 @@ public class App {
         while (!stack.isEmpty()) {
             System.out.print(stack.pop());
         }
-        System.out.println("\nThe end of program!");
     }
 }
--- src/test/java/org/example/StackImplTest.java
+++ src/test/java/org/example/StackImplTest.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import org.example.stack.Stack;
 import org.example.stack.StackImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -8,24 +11,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StackImplTest {
 
+    private Stack stack;
+
+    @BeforeEach
+    public void setUp(){
+        stack = new StackImpl();
+    }
+
     @Test
     public void newStackIsEmpty() {
-        StackImpl stack = new StackImpl();
+
 
         assertThat(stack.isEmpty(), is(true));
     }
 
     @Test
     public void afterAddAnItemStackIsNotEmpty() {
-        StackImpl stack = new StackImpl();
         stack.push(1);
+
         assertThat(stack.isEmpty(), is(false));
     }
 
     @Test
     public void afterAddAllItensStackIsNotEmpty() {
-        StackImpl stack = new StackImpl();
-
         for (int i = 0; i < 10; i++)
             stack.push(i);
 
@@ -34,14 +42,11 @@ public class StackImplTest {
 
     @Test
     public void newStackIsNotFull() {
-        StackImpl stack = new StackImpl();
-
         assertThat(stack.isFull(), is(false));
     }
 
     @Test
     public void afterAddJustSomeElementsStackShouldNotBeFull() {
-        StackImpl stack = new StackImpl();
         for (int i = 0; i < 5; i++)
             stack.push(i);
 
@@ -50,7 +55,6 @@ public class StackImplTest {
 
     @Test
     public void afterAddAllElementsStackShouldBeFull() {
-        StackImpl stack = new StackImpl();
         for (int i = 0; i < 10; i++)
             stack.push(i);
 
@@ -59,8 +63,6 @@ public class StackImplTest {
 
     @Test
     public void popRetunsTheLastInsertedItem() {
-        StackImpl stack = new StackImpl();
-
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -74,7 +76,7 @@ public class StackImplTest {
 
     @Test
     public void shouldBePossibleToCreateAStackOfOtherSide(){
-        StackImpl stackOfAHundredElements = new StackImpl(100);
+        Stack stackOfAHundredElements = new StackImpl(100);
         assertThat(stackOfAHundredElements.isEmpty(), is(true));
 
         for (int i = 0; i <= 99; i++)
@@ -84,6 +86,23 @@ public class StackImplTest {
         assertThat(stackOfAHundredElements.isEmpty(), is(false));
         for(int i = 99; i >= 0; i--)
             assertThat(stackOfAHundredElements.pop(), is(i));
+    }
+
+    @Test
+    public void pushOnAFullStack(){
+        for(int i : new int[] {1,2,3,4,5,6,7,8,9,10})
+            stack.push(i);
+
+        assertThat(stack.isFull(), is(true));
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> stack.push(11));
+
+    }
+
+    @Test
+    public void popInAnEmptyStack(){
+        assertThat(stack.isEmpty(), is(true));
+
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> stack.pop());
     }
 
 }
