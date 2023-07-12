e3129400fce3caa751ee13e147757c0bce9b0e67
luis.gomes <luis.gomes@objective.com.br>
2023-07-12 09:10:08 -0300

    add InsertionSort algorithm

---
+++ src/main/java/org/example/sort/InsertionSort.java
@@ -0,0 +1,17 @@
+package org.example.sort;
+
+public class InsertionSort {
+    public int[] order(int[] numbers) {
+        if (numbers == null || numbers.length <= 1)
+            return numbers;
+
+        for (int i = 1; i < numbers.length; i++) {
+            for (int j = i; j > 0 && numbers[j - 1] > numbers[j]; j--) {
+                int aux = numbers[j - 1];
+                numbers[j - 1] = numbers[j];
+                numbers[j] = aux;
+            }
+        }
+        return numbers;
+    }
+}
---
+++ src/test/java/org/example/sort/InsertionSortTest.java
@@ -0,0 +1,56 @@
+package org.example.sort;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class InsertionSortTest {
+
+    private InsertionSort bs;
+
+    @BeforeEach
+    void setup(){
+        bs = new InsertionSort();
+    }
+
+    @Test
+    void cantOrderEmptyArray(){
+        int[] numbers = new int[] {};
+        assertThat(bs.order(numbers), is(numbers));
+    }
+
+    @Test
+    void cantOrderNullArray(){
+        int[] numbers = null;
+        assertThat(bs.order(numbers), is(numbers));
+    }
+
+    @Test
+    void shouldOrderArrayWithOneElement(){
+        int[] numbers = new int[] {1};
+        assertThat(bs.order(numbers), is(new int[]{1}));
+    }
+
+    @Test
+    void shouldOrderArrayWithTwoElements(){
+        int[] numbers = new int[] {2,1};
+        int[] orderedNumbers = {1, 2};
+        assertThat(bs.order(numbers), is(orderedNumbers));
+    }
+
+    @Test
+    void shouldOrderArrayWithThreeElements(){
+        int[] numbers = new int[] {2,3,1};
+        int[] orderedNumbers = {1,2,3};
+        assertThat(bs.order(numbers), is(orderedNumbers));
+    }
+
+    @Test
+    void shouldOrderArrayWithLotsOfElements(){
+        int[] numbers = new int[] {2,3,1,45,342,23,75,89,56,34,68};
+        int[] orderedNumbers = {1,2,3,23,34,45,56,68,75,89,342};
+        assertThat(bs.order(numbers), is(orderedNumbers));
+    }
+}
