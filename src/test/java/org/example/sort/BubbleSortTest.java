b1b90805b7dd258038e9e731ff848a9560c4c10e
luis.gomes <luis.gomes@objective.com.br>
2023-07-18 15:20:43 -0300

    Uses the interface Sort to carry on the pointer

--- src/test/java/org/example/sort/BubbleSortTest.java
+++ src/test/java/org/example/sort/BubbleSortTest.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BubbleSortTest {
 
-    private BubbleSort bs;
+    private Sort bs;
 
     @BeforeEach
     void setup(){
--- src/test/java/org/example/sort/SelectionSortTest.java
+++ src/test/java/org/example/sort/SelectionSortTest.java
@@ -8,50 +8,50 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 class SelectionSortTest {
 
-    private SelectionSort bs;
+    private Sort ss;
 
     @BeforeEach
     void setup(){
-        bs = new SelectionSort();
+        ss = new SelectionSort();
     }
 
     @Test
     void cantOrderEmptyArray(){
         int[] numbers = new int[] {};
-        assertThat(bs.order(numbers), is(numbers));
+        assertThat(ss.order(numbers), is(numbers));
     }
 
     @Test
     void cantOrderNullArray(){
         int[] numbers = null;
-        assertThat(bs.order(numbers), is(numbers));
+        assertThat(ss.order(numbers), is(numbers));
     }
 
     @Test
     void shouldOrderArrayWithOneElement(){
         int[] numbers = new int[] {1};
-        assertThat(bs.order(numbers), is(new int[]{1}));
+        assertThat(ss.order(numbers), is(new int[]{1}));
     }
 
     @Test
     void shouldOrderArrayWithTwoElements(){
         int[] numbers = new int[] {2,1};
         int[] orderedNumbers = {1, 2};
-        assertThat(bs.order(numbers), is(orderedNumbers));
+        assertThat(ss.order(numbers), is(orderedNumbers));
     }
 
     @Test
     void shouldOrderArrayWithThreeElements(){
         int[] numbers = new int[] {2,3,1};
         int[] orderedNumbers = {1,2,3};
-        assertThat(bs.order(numbers), is(orderedNumbers));
+        assertThat(ss.order(numbers), is(orderedNumbers));
     }
 
     @Test
     void shouldOrderArrayWithLotsOfElements(){
         int[] numbers = new int[] {2,3,1,45,342,23,75,89,56,34,68};
         int[] orderedNumbers = {1,2,3,23,34,45,56,68,75,89,342};
-        assertThat(bs.order(numbers), is(orderedNumbers));
+        assertThat(ss.order(numbers), is(orderedNumbers));
     }
 
 }
