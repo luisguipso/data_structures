9d037f688a4aeca0f918dcce10394577998c9e31
luis.gomes <luis.gomes@objective.com.br>
2023-11-02 12:12:51 -0300

    clean/refactor XMLConverter

--- src/main/java/org/example/reflection/XMLConverter.java
+++ src/main/java/org/example/reflection/XMLConverter.java
@@ -1,11 +1,35 @@
 package org.example.reflection;
 
-import java.util.Arrays;
+import java.lang.reflect.Field;
 
 public class XMLConverter {
-    public String convert(Object o){
+
+    public static final String INDENT = "    ";
+    public static final String LINE_BREAK = "\n";
+
+    public String convert(Object o) {
         String className = o.getClass().getSimpleName().toLowerCase();
-        String fields = Arrays.stream(o.getClass().getDeclaredFields()).map(each -> "    <" + each.getName() + ">" + "</" + each.getName() + ">\n").reduce(String::concat).orElse("");
-        return "<"+className+">"+"\n"+ fields + "</"+className+">";
+        String fields = convertFields(o);
+        return openTag(className) + "\n" + fields + closeTag(className);
+    }
+
+    private String convertFields(Object o) {
+        StringBuilder builder = new StringBuilder();
+        for (Field each : o.getClass().getDeclaredFields()) {
+            String attributeName = each.getName();
+            builder.append(INDENT)
+                    .append(openTag(attributeName))
+                    .append(closeTag(attributeName))
+                    .append(LINE_BREAK);
+        }
+        return builder.toString();
+    }
+
+    private String openTag(String tagName) {
+        return "<" + tagName + ">";
+    }
+
+    private String closeTag(String tagName) {
+        return "</" + tagName + ">";
     }
 }
