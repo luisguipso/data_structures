7bf8f1f17f31479e87fffcb3fc1d5b5ba2a3c6c0
luis.gomes <luis.gomes@objective.com.br>
2023-11-02 12:38:25 -0300

    add Custom Object fields to xml convertion

--- src/main/java/org/example/reflection/XMLConverter.java
+++ src/main/java/org/example/reflection/XMLConverter.java
@@ -1,10 +1,11 @@
 package org.example.reflection;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
 
 public class XMLConverter {
 
-    public static final String INDENT = "    ";
     public static final String LINE_BREAK = "\n";
 
     public String convert(Object o) throws IllegalAccessException {
@@ -15,15 +16,20 @@ public class XMLConverter {
 
     private String convertFields(Object o) throws IllegalAccessException {
         StringBuilder builder = new StringBuilder();
+        List<String> javaTypes = Arrays.asList("int", "long", "float", "double", "boolean", "char", "string");
         for (Field each : o.getClass().getDeclaredFields()) {
-            if(!each.canAccess(o))
+            if (!each.canAccess(o))
                 each.trySetAccessible();
             String attributeName = each.getName();
-            builder.append(INDENT)
-                    .append(openTag(attributeName))
-                    .append(each.get(o))
-                    .append(closeTag(attributeName))
-                    .append(LINE_BREAK);
+            if (!javaTypes.contains(each.getType().getSimpleName().toLowerCase()))
+                builder.append(convert(each.get(o)))
+                        .append(LINE_BREAK);
+            else
+                builder.append(openTag(attributeName))
+                       .append(each.get(o))
+                       .append(closeTag(attributeName))
+                       .append(LINE_BREAK);
+
         }
         return builder.toString();
     }
--- src/test/java/org/example/reflection/XMLConverterTest.java
+++ src/test/java/org/example/reflection/XMLConverterTest.java
@@ -21,12 +21,26 @@ class XMLConverterTest {
         assertThat(converter.convert(user),
                 is("""
                 <user>
-                    <id>1</id>
-                    <username>luis</username>
-                    <password>123456</password>
+                <id>1</id>
+                <username>luis</username>
+                <password>123456</password>
                 </user>"""));
-//        <id>1</id>
-//                    <username>luis</username>
-//                    <password>123456</password>
+    }
+
+    @Test
+    void convertAComposedClass() throws IllegalAccessException {
+        User user = new User(1L, "luis", "123456");
+        People people = new People(1L, "Luis", user);
+        assertThat(converter.convert(people),
+                is("""
+                <people>
+                <id>1</id>
+                <name>Luis</name>
+                <user>
+                <id>1</id>
+                <username>luis</username>
+                <password>123456</password>
+                </user>
+                </people>"""));
     }
 }
