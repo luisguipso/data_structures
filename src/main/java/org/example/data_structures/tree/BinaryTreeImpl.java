b88486c6737a3b7a1a9ad0fbb75fea7a55f542f8
Luis Gomes <lguilherme@wiley.com>
2024-02-13 09:51:19 -0300

    refactor BinaryTreeImpl reorder methods and fix access modifiers

--- src/main/java/org/example/data_structures/tree/BinaryTreeImpl.java
+++ src/main/java/org/example/data_structures/tree/BinaryTreeImpl.java
@@ -19,29 +19,37 @@ public class BinaryTreeImpl implements BinaryTree {
         if (isEmpty()) this.value = n;
         else addOnSubTrees(n);
     }
-    public void addOnSubTrees(int n) {
+
+    @Override
+    public boolean isEmpty() {
+        return this.value == null;
+    }
+
+    private void addOnSubTrees(int n) {
         if (isBiggerThanThis(n)) addOnRight(n);
         else if (isLessThanThis(n)) addOnLeft(n);
     }
-    public boolean isBiggerThanThis(int n) {
+
+    private boolean isBiggerThanThis(int n) {
         return n > this.value;
     }
 
-    public void addOnRight(int n) {
+    private void addOnRight(int n) {
         if (hasRightElement()) this.right.add(n);
         else this.right = new BinaryTreeImpl(n);
     }
-    public boolean isLessThanThis(int n) {
-        return n < this.value;
-    }
 
-    public void addOnLeft(int n) {
-        if (hasLeftElement()) this.left.add(n);
-        else this.left = new BinaryTreeImpl(n);
-    }
     @Override
     public boolean hasRightElement() {
         return this.right != null;
+    }
+
+    private boolean isLessThanThis(int n) {
+        return n < this.value;
+    }
+    private void addOnLeft(int n) {
+        if (hasLeftElement()) this.left.add(n);
+        else this.left = new BinaryTreeImpl(n);
     }
 
     @Override
@@ -58,16 +66,12 @@ public class BinaryTreeImpl implements BinaryTree {
         else return 0;
     }
 
-    public boolean mustBeOnLeft(int n) {
-        return isLessThanThis(n) && hasLeftElement();
-    }
-
-    public boolean mustBeOnRight(int n) {
+    private boolean mustBeOnRight(int n) {
         return isBiggerThanThis(n) && hasRightElement();
     }
-    @Override
-    public boolean isEmpty() {
-        return this.value == null;
+
+    private boolean mustBeOnLeft(int n) {
+        return isLessThanThis(n) && hasLeftElement();
     }
 
     @Override
